17eb249d08562e692a03cd3ef449e8861224e03e
ldress <ldress@uw.edu>
2024-12-05 12:06:44 -0800

    Update data_loader.py

--- src/pyvisdmc/utils/data_loader.py
+++ src/pyvisdmc/utils/data_loader.py
@@ -2,5 +2,5 @@ import pyvibdmc as pv
 import h5py
 
 def load_data(path_to_data):
-    sim_data = pv.SimInfo(f'{path_to_data}/H5O3_0_sim_info.hdf5')
+    sim_data = pv.SimInfo(f'{path_to_data}/H2O_0_sim_info.hdf5')
     return sim_data
