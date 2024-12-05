826ec352b619dee5fb21c2e494f578aede8eed67
ldress <ldress@uw.edu>
2024-12-04 23:46:21 -0800

    created main function and added placeholder files for tests

--- pyvisdmc/main.py
+++ pyvisdmc/main.py
@@ -1,5 +1,24 @@
 import argparse
 import yaml
-# from pyvisdmc.plots import plot_name_1, plot_name_2,...
+from pyvisdmc.utils.data_loader import load_data
+from pyvisdmc.plots.eref  import plot_eref
 
-# define some functions to parse arguments from YAML file
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('config', help='path to the YAML configuration file.')
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+    with open(args.config, 'r') as file:
+        config = yaml.safe_load(file)
+
+    path_to_data = config.get('path_to_data')
+    sim_data = load_data(path_to_data)
+
+    plots = config.get('plots', [])
+    if 'eref' in plots:
+        plot_eref(sim_data)
+
+if __name__ == '__main__':
+    main()
