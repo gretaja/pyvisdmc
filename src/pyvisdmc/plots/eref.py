2b5d066195dc17789fd80b24d98aa4685a8e6ef0
Greta Jacobson <gretaja@uw.edu>
2024-12-08 19:31:11 -0800

    update data_loader and rewrite plotting functions accordingly

--- src/pyvisdmc/plots/eref.py
+++ src/pyvisdmc/plots/eref.py
@@ -3,38 +3,14 @@ import matplotlib
 matplotlib.use('Agg')  # Use a non-interactive backend
 import matplotlib.pyplot as plt
 import seaborn as sns
-import h5py
-import pandas as pd
-
-import pyvibdmc as pv
 
 sns.set_style("white")
 
-def plot_eref(data_path,molecule,sim_num,walkers,timesteps,start,stop):
+def plot_eref(molecule,sim_num,sim_data,start,stop):
     """Saves a .png of a line plot of the average enesemble energy with a simulation,
     calculates the zero point energy (ZPE) over a specified start and stopping point"""
-    if molecule == 'h5o3':
-        name = 'H5O3'
-
-    elif molecule == 'h2o':
-        name = 'H2O'
-
-    else:
-        raise ValueError('Not a valid molecule name')
     
-    if stop > timesteps:
-        raise ValueError('Stopping point exceeds length of simulation')
-    else:
-        pass
-
-    path_to_data = f'{data_path}/{molecule}_example_data/1.0w_{walkers}_walkers_{timesteps}t_1dt' #path to the folder containing the simulation data
-    
-    print(f"Creating eref plot from time {start} to time {stop} for file {path_to_data}...")
-    
-    sim_data = pv.SimInfo(f'{path_to_data}/{name}_{sim_num}_sim_info.hdf5') #name of the simulation summary file
-
-    #start = 10000 #where we want to start averaging the energy from
-    #stop = 50000 #where we want to average until
+    print(f"Creating eref plot from time {start} to time {stop} for {molecule}...")
 
     vref = sim_data.get_vref(ret_cm=True) #generates an array of the timesteps and the average energy of the ensemble at that step
     ZPE = np.mean(vref[start:stop][:,1]) #calculate the average energy in the relevant range of time steps (while the energy is stable)
@@ -47,7 +23,6 @@ def plot_eref(data_path,molecule,sim_num,walkers,timesteps,start,stop):
 
     plt.ylabel('Eref (cm$^{-1}$)')
     plt.xlabel('Timestep (1 a.u.)')
-    #plt.ylim(7000,15000)
     plt.savefig(f'{molecule}_sim_{sim_num}_zpe.png',bbox_inches='tight')
 
     plt.clf()
--- src/pyvisdmc/plots/mult_dist.py
+++ src/pyvisdmc/plots/mult_dist.py
@@ -3,31 +3,20 @@ import matplotlib
 matplotlib.use('Agg')  # Use a non-interactive backend
 import matplotlib.pyplot as plt
 import seaborn as sns
-import h5py
-import pandas as pd
-
-import pyvibdmc as pv
 
 sns.set_style("white")
 
-def plot_dists(data_path,molecule,sim_num,walkers,timesteps,start,stop,dists,hist=True,line=True,exp=True):
+def plot_dists(molecule,sim_num,analyzer,weights,dists,hist=True,line=True,exp=True):
     """Saves a .png of a histogram and/or line plot of a given distribtion of geometries
       over a specified start and stopping point in the DMC simulation"""
     if molecule == 'h5o3':
-        name = 'H5O3'
         num_atoms = 8
 
     elif molecule == 'h2o':
-        name = 'H2O'
         num_atoms = 3
 
     else:
         raise ValueError('Not a valid molecule name')
-    
-    if stop > timesteps:
-        raise ValueError('Stopping point exceeds length of simulation')
-    else:
-        pass
 
     for d in range(len(dists)):
         for ind in dists[d]:
@@ -36,20 +25,8 @@ def plot_dists(data_path,molecule,sim_num,walkers,timesteps,start,stop,dists,his
         
             else:
                 pass
-
-    path_to_data = f'{data_path}/{molecule}_example_data/1.0w_{walkers}_walkers_{timesteps}t_1dt' #path to the folder containing the simulation data
     
-    print(f"Creating plot mult_dist for dists {dists} from file {path_to_data}...")
-    
-    sim_data = pv.SimInfo(f'{path_to_data}/{name}_{sim_num}_sim_info.hdf5') #name of the simulation summary file
-    snapshots = np.arange(start,stop,1000) #pull data every 1000 time steps from 10,000 to 50,000
-
-    coords, weights = sim_data.get_wfns(snapshots) #load in the molecule geometries (coords) and their associated weights
-    coords = pv.Constants.convert(coords,'angstroms',to_AU=False) # conversion of coordinates from atomic units to Angstroms (more common in chemistry)
-
-    analyzer = pv.AnalyzeWfn(coords)
-
-    #df = pd.DataFrame({'weights': dws})
+    print(f"Creating plot mult_dist for dists {dists} for {molecule}...")
 
     dist_vals = []
     exp_vals = []
