3a2a794f886b65f684af0c95ea6edb5183f524b9
Greta Jacobson <gretaja@uw.edu>
2024-12-09 03:52:55 -0800

    update tests to call pre-loaded data

--- tests/test_mult_dist.py
+++ tests/test_mult_dist.py
@@ -5,9 +5,9 @@ Tests for the mult_dist function
 import pytest
 import numpy as np
 
+import pyvibdmc as pv
+
 from pyvisdmc.plots import plot_dists
-from pyvisdmc.test_data import DATA_PATH
-from pyvisdmc.utils.data_loader import load_data, sim_info
 
 def test_smoke_default():
     """
@@ -15,16 +15,10 @@ def test_smoke_default():
     """
     molecule = 'h2o'
     sim_num = 0
-    walkers = 5000
-    timesteps = 20000
-    start = 5000
-    stop = 20000
-
     dists = [[0,1],[0,2]]
-
-    sim_data = load_data(DATA_PATH,molecule,sim_num,walkers,timesteps)
-
-    analyzer, weights = sim_info(sim_data,start,stop)
+    h2o_cds = np.load('src/pyvisdmc/test_data/h2o_cds.npy')
+    weights = np.load('src/pyvisdmc/test_data/h2o_dws.npy')
+    analyzer = pv.AnalyzeWfn(h2o_cds)
 
     plot_dists(molecule,sim_num,analyzer,weights,dists)
 
@@ -36,16 +30,10 @@ def test_smoke_hist_false():
     """
     molecule = 'h2o'
     sim_num = 0
-    walkers = 5000
-    timesteps = 20000
-    start = 5000
-    stop = 20000
-
     dists = [[0,1],[0,2]]
-
-    sim_data = load_data(DATA_PATH,molecule,sim_num,walkers,timesteps)
-
-    analyzer, weights = sim_info(sim_data,start,stop)
+    h2o_cds = np.load('src/pyvisdmc/test_data/h2o_cds.npy')
+    weights = np.load('src/pyvisdmc/test_data/h2o_dws.npy')
+    analyzer = pv.AnalyzeWfn(h2o_cds)
 
     plot_dists(molecule,sim_num,analyzer,weights,dists,hist=False)
 
@@ -57,16 +45,10 @@ def test_smoke_line_false():
     """
     molecule = 'h2o'
     sim_num = 0
-    walkers = 5000
-    timesteps = 20000
-    start = 5000
-    stop = 20000
-
     dists = [[0,1],[0,2]]
-
-    sim_data = load_data(DATA_PATH,molecule,sim_num,walkers,timesteps)
-
-    analyzer, weights = sim_info(sim_data,start,stop)
+    h2o_cds = np.load('src/pyvisdmc/test_data/h2o_cds.npy')
+    weights = np.load('src/pyvisdmc/test_data/h2o_dws.npy')
+    analyzer = pv.AnalyzeWfn(h2o_cds)
 
     plot_dists(molecule,sim_num,analyzer,weights,dists,line=False)
 
@@ -78,44 +60,14 @@ def test_smoke_exp_false():
     """
     molecule = 'h2o'
     sim_num = 0
-    walkers = 5000
-    timesteps = 20000
-    start = 5000
-    stop = 20000
-
     dists = [[0,1],[0,2]]
-
-    sim_data = load_data(DATA_PATH,molecule,sim_num,walkers,timesteps)
-
-    analyzer, weights = sim_info(sim_data,start,stop)
+    h2o_cds = np.load('src/pyvisdmc/test_data/h2o_cds.npy')
+    weights = np.load('src/pyvisdmc/test_data/h2o_dws.npy')
+    analyzer = pv.AnalyzeWfn(h2o_cds)
 
     plot_dists(molecule,sim_num,analyzer,weights,dists,exp=False)
 
     return
-
-def test_molecule_name():
-    """
-    Edge test for invalid molecule name
-    """
-    with pytest.raises(
-        ValueError, match="Not a valid molecule name"
-    ):
-        molecule = 'h3o'
-        sim_num = 0
-        walkers = 5000
-        timesteps = 20000
-        start = 5000
-        stop = 20000
-
-        dists = [[0,1],[0,2]]
-
-        sim_data = load_data(DATA_PATH,molecule,sim_num,walkers,timesteps)
-
-        analyzer, weights = sim_info(sim_data,start,stop)
-
-        plot_dists(molecule,sim_num,analyzer,weights,dists)
-
-    return 
 
 def test_atom_indices():
     """
@@ -126,16 +78,10 @@ def test_atom_indices():
     ):
         molecule = 'h2o'
         sim_num = 0
-        walkers = 5000
-        timesteps = 20000
-        start = 5000
-        stop = 20000
-
         dists = [[0,4],[0,2]]
-
-        sim_data = load_data(DATA_PATH,molecule,sim_num,walkers,timesteps)
-
-        analyzer, weights = sim_info(sim_data,start,stop)
+        h2o_cds = np.load('src/pyvisdmc/test_data/h2o_cds.npy')
+        weights = np.load('src/pyvisdmc/test_data/h2o_dws.npy')
+        analyzer = pv.AnalyzeWfn(h2o_cds)
 
         plot_dists(molecule,sim_num,analyzer,weights,dists)
 
